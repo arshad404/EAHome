079e8a31ad19bf57fe5be55a4278da638a37c782
Arshad <mohammad.1610093@kiet.edu>
2021-06-12 10:10:34 +0530

    intial setup

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ const db = require("./config/dbConnect");
 
 require("dotenv").config();
 
+//check db either connected or not
 db.authenticate()
   .then(() => console.log("Database connected..."))
   .catch((err) => console.log("Error in connecting database", err));
 
+//starting the server
 app.listen(process.env.PORT, () => {
   console.log("server started");
 });
