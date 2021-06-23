26ceb567d72cc03ab40876fd8cd1093f6c9b8836
Arshad <mohammad.1610093@kiet.edu>
2021-06-23 14:04:48 +0530

    Done: JWT Done

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 require("dotenv").config();
 const db = require("../models");
 const Users = db.users;
@@ -16,8 +17,34 @@ module.exports.signup = async (req, res, next) => {
       trainer: trainer,
     });
     res.status(200).json({ Status: "User added successfully" });
-    console.log("User created successfully");
   } catch (err) {
-    console.log(err);
+    let error = new Error(`Problem in signup: ${err.message}`);
+    error.status = 403;
+    throw error;
+  }
+};
+
+module.exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await Users.findOne({ where: { email: email } });
+    if (user) {
+      let matchPass = await bcrypt.compare(password, user.password);
+      if (matchPass) {
+        let token = user.generateToken();
+        return res
+          .status(200)
+          .header("x-auth-token", token)
+          .json({ Token: token });
+      } else {
+        return res.status(400).json({ message: "Wrong Credentials" });
+      }
+    } else {
+      return res.status(400).json({ message: "Wrong email/pass" });
+    }
+  } catch (err) {
+    let error = new Error(`Problem in login ${err.message}`);
+    error.status = 403;
+    throw error;
   }
 };
